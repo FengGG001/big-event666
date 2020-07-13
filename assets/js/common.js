5416ca3f26e196537c86baed4da4830d75b0b4b3
XiaoZhaoXianSheng <2292868435@qq.com>
2020-07-13 16:14:29 +0800

    完成了退出功能和全部的身份认证功能统一配置了ajax请求

---
+++ assets/js/common.js
@@ -0,0 +1,17 @@
+$.ajaxPrefilter(function (options) {
+    // 修改url
+    options.url = 'http://www.liulongbin.top:3007' + options.url;
+    // 统一配置 tomplete
+    options.complete = function (xhr) {
+        if (xhr.responseJSON.status === 1 && xhr.responseJSON.message === '身份认证失败！') {
+            // 删除 token 
+            localStorage.removeItem('token');
+            // 跳转页面到 login.html
+            location.href = '/login.html'
+        }
+    }
+    // 统一配置headers
+    options.headers = {
+        Authorization: localStorage.getItem('token'),
+    }
+})
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,37 @@
 // ------------------------- 获取用户信息，并且渲染页面------------------------------------------------
 function getUserInfo() {
     $.ajax({
-        url: 'http://www.liulongbin.top:3007/my/userinfo',
+        url: '/my/userinfo',
         success: function (res) {
-            console.log(res)
-            // 渲染页面  1、设置欢迎语
-            var name = res.data.nickname || res.data.username;
-            $('#username').html('&nbsp;&nbsp;' + name)
-            // 2.设置头像
-            if (res.data.user_pic) {
-                // 有图片  使用头像
-                $('.layui-nav-img').attr('src', res.data.user_pic).show();
-                $('.txt-avater').hide();
-            } else {
-                // 没有图片，使用账号
-                var firstWord = name.substr(0, 1).toUpperCase();
-                $('.txt-avater').text(firstWord).css('display', 'inline-block');
-                $('.layui-nav-img').hide();
+            if (res.status === 0) {
+                // console.log(res)
+                // 渲染页面  1、设置欢迎语  如果用户有名称使用用户名，，没有那么使用用户的账号名称
+                var name = res.data.nickname || res.data.username;
+                $('#username').html('&nbsp;&nbsp;' + name)
+                // 2.设置头像
+                if (res.data.user_pic) {
+                    // 有图片  使用头像
+                    $('.layui-nav-img').attr('src', res.data.user_pic).show();
+                    $('.txt-avater').hide();
+                } else {
+                    // 没有图片，使用账号
+                    var firstWord = name.substr(0, 1).toUpperCase();
+                    $('.txt-avater').text(firstWord).css('display', 'inline-block');
+                    $('.layui-nav-img').hide();
+                }
             }
         },
-        headers: {
-            Authorization: localStorage.getItem('token'),
-        }
     })
 };
 getUserInfo();
+
+// --------------------------------------- 退出---------------------------------------
+// 思路  删除本地存储的字符串，跳转 login 页面
+$('#logout').click(function () {
+    layer.confirm('确定退出吗', function (index) {
+        // 删除 token 
+        localStorage.removeItem('token');
+        // 跳转页面到 login.html
+        location.href = '/login.html';
+    })
+})
